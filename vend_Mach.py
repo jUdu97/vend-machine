9d832cb3d63700c535281c1b2f0361bc9e9a0f45
juduehi <juduehi@iu.edu>
2019-06-09 03:46:23 -0400

    print options

---
+++ vend_Mach.py
@@ -0,0 +1,8 @@
+def vendM():
+    print("Oreo Pack \t\tCheez-its \t\Snickers \n")
+    print("Fudge Rounds \t\tTwizzlers \tSour Patch Kids \n")
+    print("Skittles \t\tM & Ms \t\tBabe Ruth \n")
+    print("Hubba Bubba Max \tSnickers \tSlim Jim \n")
+    print("Hot Cheetos \t\t5 Gum \t\tAnimal Crackers \n")
+
+    return None
