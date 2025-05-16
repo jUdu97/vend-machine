f42e6dd8bc66b6c605b5d8b88470eef5f1b96e2c
jUdu97 <juduehi41@gmail.com>
2025-05-16 13:58:29 -0500

    Added print_welcome method

--- VendingMachine.py
+++ VendingMachine.py
@@ -4,3 +4,10 @@ class VendingMachine:
 
     def __str__(self):
         return f"{self.name}"
+
+    def print_welcome(self):
+        """ Prints welcome message to vending machine """
+        print('Welcome to '+self.name+' Vending Machine!!\n')
+        print('-' * 70)
+        print('ONLY ACCEPTS QUARTERS ($0.25)!')
+        print('-' * 70)
