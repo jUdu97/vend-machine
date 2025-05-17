c07f61f6ffc987cbce07eb152547c78f8b30071c
jUdu97 <juduehi41@gmail.com>
2025-05-17 17:05:21 -0500

    added testing file

--- vending_machine.py
+++ vending_machine.py
@@ -15,7 +15,6 @@ class VendingMachine:
 
     def show_options(self):
         """ Prints options available in vending machine """
-        print('-' * 70)
         print('{:<20} {:<20} {:<20}\n'.format('Oreo Pack (A1)', 'Cheez-its (A2)', 'Snickers (A3)'))
         print('{:<20} {:<20} {:<20}\n'.format('Fudge Rounds (B1)', 'Twizzlers (B2)', 'Sour Patch Kids (B3)'))
         print('{:<20} {:<20} {:<20}\n'.format('Skittles (C1)', 'M & Ms (C2)', 'Babe Ruth (C3)'))
